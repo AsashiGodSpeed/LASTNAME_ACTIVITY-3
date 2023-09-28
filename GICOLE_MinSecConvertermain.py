52192ca99276e944d426cb5852873a82c6b1aa6c
Aldrick James A. Gicole <144977698+AsashiGodSpeed@users.noreply.github.com>
2023-09-28 21:10:06 +0800

    Update GICOLE_MinSecConvertermain.py

--- GICOLE_MinSecConvertermain.py
+++ GICOLE_MinSecConvertermain.py
@@ -1,9 +1,9 @@
 # Enter
-ammount_time = int(input("Enter the amount of time in seconds: "))
+ammount_time = int(input("Enter an integer in seconds: "))
 
 # Calculate minutes and seconds
 minutes = ammount_time // 60
 seconds = ammount_time % 60
 
 # Display the result and i dont know why in all letters why F/f is the one able to do like that
-print(F"{total_seconds}seconds is equal to {minutes} minutes and {seconds} seconds.")
+print(f"{ammount_time}seconds is {minutes} minutes and {seconds} seconds.")
